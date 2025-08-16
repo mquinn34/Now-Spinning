13f11c216a7a5b33eed89320b60e5acad90d403d
Mike Quinn <mikequinn@Mikes-MacBook-Air.local>
2025-08-16 12:16:51 -0500

    Add Cloudinary storage backend for image uploads

--- project/settings.py
+++ project/settings.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
     "pages",
     "accounts",
     "spins",
+     "cloudinary",
+    "cloudinary_storage",
 ]
 
 MIDDLEWARE = [
@@ -133,3 +135,13 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
 ]
+
+
+
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": os.environ.get("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": os.environ.get("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.environ.get("CLOUDINARY_API_SECRET"),
+}
+
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
